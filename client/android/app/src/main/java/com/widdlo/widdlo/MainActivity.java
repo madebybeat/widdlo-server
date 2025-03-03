8885166dd077e53c9d78af690f0008c31bab3a2f
Ailakks <hola@ailakks.dev>
2025-03-03 22:14:54 +0100

    refactor: root source

--- client/android/app/src/main/java/com/widdlo/widdlo/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.widdlo.widdlo;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- client/src/components/ui/general/error/not-found/not-found.jsx
+++
@@ -1,8 +0,0 @@
-import Error from "../error/error";
-
-export default function NotFound() {
-
-    return (
-        <Error title="¡Nada por aquí!" body="Hemos buscado por todas partes, de esquina a esquina, incluso por debajo de los sofás, pero no hemos encontrado nada." />
-    );
-}
